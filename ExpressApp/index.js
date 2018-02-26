25befc60e2eb05475cfbac7a6c4b0ebaa7c2837b
Cedric <pagecedric@gmail.com>
2018-02-26 15:33:30 +0100

    init expressapp

---
+++ ExpressApp/index.js
@@ -0,0 +1,12 @@
+var express= require('express');
+var app=express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World');
+}).get('/fr', function(req, res) {
+    res.send('Bonjour monde');
+})
+
+app.listen(8081, () => {
+    console.log("Express app running on 8081");
+})
